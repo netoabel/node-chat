7a239926edf92a582723e3f7a339594ea397438c
GuiLucena <lucena9682@gmail.com>
2015-06-02 09:01:24 -0300

    small change

--- src/chat/lib/index.js
+++ src/chat/lib/index.js
@@ -1,25 +1,25 @@
 'use strict';
 
 var http = require('http').Server(),
-    io = require('socket.io')(http),
+  io = require('socket.io')(http),
 
-    ConnectionDAO = require('./model/dal/connection.dao'),
-    SocketioConnectionDAO = require('./model/dal/socketio/socketio-connection.dao'),
-    MessageDAO = require('./model/dal/message.dao'),
-    SocketioMessageDAO = require('./model/dal/socketio/socketio-message.dao'),
-    StubUserDAO = require('./model/dal/stub/stub-user.dao'),
-    UserDAO = require('./model/dal/user.dao'),
-    User = require('./model/user'),
-    UserList = require('./model/user-list'),
+  ConnectionDAO = require('./model/dal/connection.dao'),
+  SocketioConnectionDAO = require('./model/dal/socketio/socketio-connection.dao'),
+  MessageDAO = require('./model/dal/message.dao'),
+  SocketioMessageDAO = require('./model/dal/socketio/socketio-message.dao'),
+  StubUserDAO = require('./model/dal/stub/stub-user.dao'),
+  UserDAO = require('./model/dal/user.dao'),
+  User = require('./model/user'),
+  UserList = require('./model/user-list'),
 
-    userList = new UserList(),
-    userDAO = new UserDAO(),
-    connectionDAO = new ConnectionDAO(userList, userDAO),
-    messageDAO = new MessageDAO(),
+  userList = new UserList(),
+  userDAO = new UserDAO(),
+  connectionDAO = new ConnectionDAO(userList, userDAO),
+  messageDAO = new MessageDAO(),
 
-    stubUserDAO = new StubUserDAO(userDAO),
-    socketioConnectionDAO = new SocketioConnectionDAO(io, connectionDAO, messageDAO),
-    socketioMessageDAO = new SocketioMessageDAO(io, messageDAO);
+  stubUserDAO = new StubUserDAO(userDAO),
+  socketioConnectionDAO = new SocketioConnectionDAO(io, connectionDAO, messageDAO),
+  socketioMessageDAO = new SocketioMessageDAO(io, messageDAO);
 
 http.listen(3000, function () {
   console.log('WebSocket server listening for connections on port 3000');
